7e3733a8353e5ce2b2ef8fd152770b76d9f745d1
Kos <koshuang@gmail.com>
2021-07-03 19:53:45 +0800

    style(my-resume): add styles and font icon for material design

--- examples/my-resume/src/App.tsx
+++ examples/my-resume/src/App.tsx
@@ -7,6 +7,11 @@ function App(): JSX.Element {
   return (
     <StyledThemeProvider>
       <Provider store={store}>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <div className="App">
           <Home />
         </div>
