a8adca9d62c6a13a7bd8a773f72397279a3d14cb
Kos <koshuang@gmail.com>
2021-07-03 19:22:17 +0800

    refactor(my-app/component): remove default components

--- examples/my-resume/src/components/logo/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ReactIcon } from "components/icons";
-
-export const Logo: React.FC = () => {
-    return (
-            <ReactIcon   data-test="icon"  className="text-white mx-auto" width="96" height="58" />
-    );
-};
